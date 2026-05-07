73d56cfd849815312ba7d0c262abc3856193f253
proshanto7 <proshantosarker112@gmail.com>
2026-05-08 04:11:11 +0600

    company model create

---
+++ model/company.model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const companySchema = new Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+    },
+    image: {
+      type: String,
+      required: [true, "image is required"],
+    },
+  },
+  { timestamps: true, versionKey: false },
+);
+module.exports = mongoose.model("Company", companySchema);
