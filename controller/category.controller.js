3992e8568bd154d0ddad1303522c53a73784bfef
proshanto7 <proshantosarker112@gmail.com>
2026-05-08 19:47:16 +0600

    add category controller logic apply

--- controller/category.controller.js
+++ controller/category.controller.js
@@ -1,5 +1,26 @@
+const { deleteFile } = require("../helpers/deleteHelper");
+const categoryModel = require("../model/category.model");
+const { apiResponse } = require("../utils/apiResponse");
 const { asyncHandler } = require("../utils/asyncHandler");
 
 exports.addCategoryController = asyncHandler(async (req, res) => {
-  res.send("add category");
+  const { name, isActive } = req.body;
+  const image = req.file?.filename;
+
+  const existsCategory = await categoryModel.findOne({ name });
+  if (existsCategory) {
+    deleteFile(image);
+    return apiResponse(res, 409, "category already exists");
+  }
+  if (!name) {
+    deleteFile(image);
+    return apiResponse(res, 400, "category name is required");
+  }
+  const category = new categoryModel({
+    name,
+    image: `${process.env.UPLOADS_BASE_URL}/${image}`,
+    isActive,
+  });
+  await category.save();
+  apiResponse(res, 200, "category added successfully", category);
 });
